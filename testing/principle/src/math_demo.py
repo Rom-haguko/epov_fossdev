08295944797260b2adfaf1a31e2afc82448e727f
RomG <romagureev170@gmail.com>
2026-03-05 14:32:52 +0800

    Added new test in funcion calculate_tax

--- testing/principle/src/math_demo.py
+++ testing/principle/src/math_demo.py
@@ -8,5 +8,9 @@ def calculate_tax_bugged(income):
     return income * 0.15
 
 def calculate_tax(income):
+    if income < 0:
+        raise ValueError(
+            "Could not have nagative income"
+        )
     return int(income * 0.15 * 100) / 100
 
