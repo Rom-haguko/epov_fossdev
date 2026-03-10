a93ad77710f08c366d7f60b585485cfaf8887a33
RomG <romagureev170@gmail.com>
2026-03-10 14:42:18 +0800

    Added boundary income test

--- testing/tdd/src/ndfl.py
+++ testing/tdd/src/ndfl.py
@@ -1,2 +1,10 @@
 def calculate_ndfl(income):
-    return income * 0.13
+    result = 0
+    if income < 2_400_00:
+        result = income * 0.13
+    else:
+        result = (
+            2_400_000 * 0.13 +
+            (income - 2_400_000) * 0.15
+        )
+    return result
